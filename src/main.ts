d7e53f7c51f7b4c7200ddf73d89cdb686ae2f017
Ryan <656040874@qq.com>
2023-02-28 12:27:52 +0800

    style(utils): hTML默认样式标准化或者重置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 // HTML对默认样式进行标准化
-import './normalize.css'
+import './styles/normalize.css'
 import App from './App.vue'
 
 console.log(import.meta.env.VITE_BASE_URL)
