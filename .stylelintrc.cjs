c2569cb56c47e4fc5c38e64d06b520e420b0ad48
Ryan <656040874@qq.com>
2023-08-25 16:06:57 +0800

    fix: .stylelintrc.cjs

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,17 +1,26 @@
 module.exports = {
   root: true,
   extends: [
-    'stylelint-config-standard', 
-    'stylelint-config-standard-scss', 
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
     'stylelint-config-recommended-vue/scss',
     'stylelint-config-recess-order',
-    'stylelint-config-prettier'
+    'stylelint-config-prettier',
   ],
   overrides: [
-		// 扫描 .vue/html 文件中的<style>标签内的样式
-		{
-			files: ["**/*.{vue,html}"],
-			customSyntax: "postcss-html"
-		}
-	],
+    // 扫描 .vue/html 文件中的<style>标签内的样式
+    {
+      files: ['**/*.{vue,html}'],
+      customSyntax: 'postcss-html',
+    },
+  ],
+  // 忽略的行上面 添加
+  /* stylelint-disable */
+  rules: {
+    'rule-empty-line-before': null,
+    'selector-class-pattern': null,
+    'no-descending-specificity': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'color-hex-case': 'lower',
+  },
 }
